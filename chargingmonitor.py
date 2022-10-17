d2ca59fca2a94eb62ba3b51985767849410bc44f
willkyu <87601487+willkyu@users.noreply.github.com>
2022-10-17 11:26:32 +0800

    Update chargingmonitor.py

--- chargingmonitor.py
+++ chargingmonitor.py
@@ -1,7 +1,7 @@
 from time import sleep
-from utils.chargingSpider import getTextFromURL
-from utils.textProcess import Text2Dict
-from utils.mailTools import CXreply, sendMail
+from chargingSpider import getTextFromURL
+from textProcess import Text2Dict
+from mailTools import CXreply, sendMail
 import pickle
 
 READJKFILE_TIME = 5 # 监控 READJKFILE_TIME秒一次
@@ -12,8 +12,18 @@ FAILDED_COUNT = 5
 user_agent_list=['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko)','Mozilla/5.0 (Windows NT 6.1; WOW64; rv:11.0) Gecko/20100101 Firefox/11.0','Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)','Opera/9.80 (Windows NT 6.1; WOW64; U; zh-cn) Presto/2.10.229 Version/11.62']
 # chargingURL='http://10.20.7.127:5000/'
 chargingURL='http://sudacharge.haoxiaoren.com'
+dshURL='http://sudacharge.haoxiaoren.com/?region=DSH'
+ychURL='http://sudacharge.haoxiaoren.com/?region=YCH'
 groupList=['东吴桥下','东七宿舍楼','本七宿舍楼','逸夫楼','文思楼']
-translateDict={'东吴桥下':'Under DongWu Bridge','东七宿舍楼':'East 7 Dormitory','本七宿舍楼':'Center 7 Dormitory','逸夫楼':'YiFu Building','文思楼':'WenSi Building','一号机':'Machine.NO.1','二号机':'Machine.NO.2','三号机':'Machine.NO.3'}
+groupList_dsh=['B02号楼','B04号楼','201号楼','109号楼','食堂西北角','104号楼']
+groupList_ych=['食堂','1C号楼','1B号楼']
+
+translateDict={'东吴桥下':'Under DongWu Bridge','东七宿舍楼':'East 7 Dormitory','本七宿舍楼':'Center 7 Dormitory','逸夫楼':'YiFu Building',\
+    '文思楼':'WenSi Building','一号机':'Machine.NO.1','二号机':'Machine.NO.2','三号机':'Machine.NO.3',\
+        '食堂':'Canteen','1C号楼':'NO.1C Building','1B号楼':'NO.1B Building','B02号楼':'NO.B02 Building','B04号楼':'NO.B04 Building',\
+            '201号楼':'NO.201 Building','109号楼':'NO.109 building','食堂西北角':'Northwest corner of Canteen','104号楼':'NO.104 building',\
+                '01180111':'Machine.01180111','01180146':'Machine.01180146','01180112':'Machine.01180112','01180193':'Machine.01180193','01180074':'Machine.01180074','01180110':'Machine.01180110',\
+                    '01180192':'Machine.01180192','01180050':'Machine.01180050','01180058':'Machine.01180058'}
 
 
 def writepickle(filename,data):
@@ -34,6 +44,8 @@ def Monitor1Epoch(userEmailList,userTargetList,userStatusList,failedCount):
 
     # 爬一次
     urlText = getTextFromURL(chargingURL)
+    urlText_dsh = getTextFromURL(dshURL)
+    urlText_ych = getTextFromURL(ychURL)
     # 爬取失败failedCount - 1
     if urlText == 'failed.':
         failedCount -= 1
@@ -42,25 +54,31 @@ def Monitor1Epoch(userEmailList,userTargetList,userStatusList,failedCount):
     failedCount = FAILDED_COUNT
 
     chargingDict,groupItemDict,tipsDict = Text2Dict(urlText, groupList)
+    chargingDict_dsh,groupItemDict_dsh,tipsDict_dsh = Text2Dict(urlText_dsh, groupList_dsh)
+    chargingDict_ych,groupItemDict_ych,tipsDict_ych = Text2Dict(urlText_ych, groupList_ych)
+    # tipsDict = tipsDict.update(tipsDict_dsh)
+    # tipsDict = tipsDict.update(tipsDict_ych)
+    chargingDictMain = {'天赐庄':chargingDict,'独墅湖':chargingDict_dsh,'阳澄湖':chargingDict_ych}
+
 
     newEmailList=[]
     newTargetList=[]
     newStatusList=[]
     for index,item in enumerate(userTargetList):
         # print(chargingDict)
-        if chargingDict[item[0]][item[1]][item[2]] != 'charging':
+        if chargingDictMain[item[0]][item[1]][item[2]][item[3]] != 'charging':
             # 充电停止
-            sendMail(userEmailList[index], '{}, {}, {} has stopped charging!'.format(translateDict[item[0]],translateDict[item[1]],item[2]))
+            sendMail(userEmailList[index], '{}, {}, {} has stopped charging!'.format(translateDict[item[1]],translateDict[item[2]],item[3]))
         
         elif userStatusList[index] - 1 == 0:
             # 计时归零
-            sendMail(userEmailList[index], '{}, {}, {} has been charged for 6 hours, stopping monitoring.'.format(translateDict[item[0]],translateDict[item[1]],item[2]))
+            sendMail(userEmailList[index], '{}, {}, {} has been charged for 6 hours, stopping monitoring.'.format(translateDict[item[1]],translateDict[item[2]],item[3]))
         else:
             # 其他计时-1
             newEmailList.append(userEmailList[index])
             newTargetList.append(userTargetList[index])
             newStatusList.append(userStatusList[index]-1)
-    return chargingDict,groupItemDict,tipsDict,failedCount,\
+    return chargingDictMain,groupItemDict,tipsDict,failedCount,\
         newEmailList,newTargetList,newStatusList
 
 if __name__ == '__main__':
@@ -76,17 +94,17 @@ if __name__ == '__main__':
     chargingDict={}
     groupItemDict={}
     tipsDict={}
-    chargingDict={}
+    chargingDictMain={}
 
-    JK = '/anaconda/pythoncode/ChargingMonitor/JK.pkl'
-    CX = '/anaconda/pythoncode/ChargingMonitor/CX.pkl'
+    JK = 'JK.pkl'
+    CX = 'CX.pkl'
     while 1:
         # 爬虫 SPIDER_TIME秒一次
         if failedCount==0:
             # 连续爬取失败FAILED_COUNT次，服务器可能在维修
             spiderCount = 300
         if spiderCount == SPIDER_TIME:
-            chargingDict0,groupItemDict0,tipsDict0,failedCount,\
+            chargingDictMain0,groupItemDict0,tipsDict0,failedCount,\
                 newEmailList,newTargetList,newStatusList=\
                     Monitor1Epoch(userEmailList,userTargetList,userStatusList,failedCount)
             if failedCount==5:
@@ -94,33 +112,35 @@ if __name__ == '__main__':
                 userEmailList = newEmailList
                 userTargetList = newTargetList
                 userStatusList = newStatusList
-                chargingDict,groupItemDict,tipsDict = chargingDict0,groupItemDict0,tipsDict0
+                chargingDictMain,groupItemDict,tipsDict = chargingDictMain0,groupItemDict0,tipsDict0
             pass
         elif spiderCount==0:
             spiderCount = SPIDER_TIME+1
             
         CXlist = readpickle(CX)
-        for [itemEmail,itemGroup] in CXlist:
-            CXreply(itemEmail,chargingDict[itemGroup],itemGroup,tipsDict)
+        for [itemEmail,itemXQ,itemGroup] in CXlist:
+            CXreply(itemEmail,chargingDictMain[itemXQ][itemGroup],itemGroup,tipsDict)
         clearList=[]
         writepickle(CX,clearList)
     
         if readJKFileCount==READJKFILE_TIME:
             JKlist=readpickle(JK)
             for [itemEmail,itemTarget] in JKlist:
-                if chargingDict[itemTarget[0]][itemTarget[1]][itemTarget[2]] == 'offline':
-                    sendMail(itemEmail,'{}, {}, {} is offline! please change pile.'.format(translateDict[itemTarget[0]],translateDict[itemTarget[1]],itemTarget[2]))
+                if itemTarget[3] not in chargingDictMain[itemTarget[0]][itemTarget[1]][itemTarget[2]]:
+                    sendMail(itemEmail,'{}, {}, {} is offline or not existed! please change pile.'.format(translateDict[itemTarget[1]],translateDict[itemTarget[2]],itemTarget[3]))
+                elif chargingDictMain[itemTarget[0]][itemTarget[1]][itemTarget[2]][itemTarget[3]] == 'offline':
+                    sendMail(itemEmail,'{}, {}, {} is offline! please change pile.'.format(translateDict[itemTarget[1]],translateDict[itemTarget[2]],itemTarget[3]))
                 elif (itemEmail in userEmailList) and (userTargetList[userEmailList.index(itemEmail)] != itemTarget):
                     userTargetList[userEmailList.index(itemEmail)] = itemTarget
                     userStatusList[userEmailList.index(itemEmail)] = MAX_TIME
-                    sendMail(itemEmail,'You have changed your monitor target to: {}, {}, {}.'.format(translateDict[itemTarget[0]],translateDict[itemTarget[1]],itemTarget[2]))
+                    sendMail(itemEmail,'You have changed your monitor target to: {}, {}, {}.'.format(translateDict[itemTarget[1]],translateDict[itemTarget[2]],itemTarget[3]))
                 elif itemEmail not in userEmailList:
                     userEmailList.append(itemEmail)
                     userTargetList.append(itemTarget)
                     userStatusList.append(MAX_TIME)
-                    sendMail(itemEmail,'Start Monitoring with target: {}, {}, {}.'.format(translateDict[itemTarget[0]],translateDict[itemTarget[1]],itemTarget[2]))
+                    sendMail(itemEmail,'Start Monitoring with target: {}, {}, {}.'.format(translateDict[itemTarget[1]],translateDict[itemTarget[2]],itemTarget[3]))
                 else:
-                    sendMail(itemEmail,'Do not submit again! You have been monitoring target:{}, {}, {}.'.format(translateDict[itemTarget[0]],translateDict[itemTarget[1]],itemTarget[2]))
+                    sendMail(itemEmail,'Do not submit again! You have been monitoring target:{}, {}, {}.'.format(translateDict[itemTarget[1]],translateDict[itemTarget[2]],itemTarget[3]))
 
             clearList=[]
             writepickle(JK,clearList)
